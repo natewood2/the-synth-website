b8dbc423b845c95c487f958a4eafbd0e4b7f5837
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-13 15:34:02 -0500

    Working on the premade sequences feature

---
+++ static/scripts/sequences.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let fastBtn = document.getElementById('fast');
+  let chillBtn = document.getElementById('chill');
+  let spaceBtn = document.getElementById('space');
+
+  const buttonState = {
+    'fast': false,
+    'chill': false,
+    'space': false,
+  }
+  fastBtn.addEventListener('click', function () {
+    if (buttonState['fast'] === false) {
+      //Turning on
+      console.log('Fast on');
+      buttonState['fast'] = true;
+    } else if (buttonState['fast'] === true){
+      // Turning off
+      console.log('Fast off');
+      buttonState['fast'] = false;
+    }
+  });
+  chillBtn.addEventListener('click', function () {
+    if (buttonState['chill'] === false) {
+      //Turning on
+      console.log('chill on');
+      buttonState['chill'] = true;
+    } else if (buttonState['chill'] === true) {
+      // Turning off
+      console.log('chill off');
+      buttonState['chill'] = false;
+    }
+  });
+  spaceBtn.addEventListener('click', function () {
+    if (buttonState['space'] === false) {
+      //Turning on
+      console.log('space on');
+      buttonState['space'] = true;
+    } else if (buttonState['space'] === true) {
+      // Turning off
+      console.log('space off');
+      buttonState['space'] = false;
+    }
+  });
+});

bacd71784953758f40852fc2cb9492c58cff33ef
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-14 09:21:03 -0500

    Added feature to save and load weather state on page load

--- static/scripts/menu.js
+++ static/scripts/menu.js
@@ -46,8 +46,30 @@ export function menuScenesSelector(scene, renderer) {
     'snowToggle': false
   };
 
+  // Load saved state from local storage
+  const savedState = JSON.parse(localStorage.getItem('activeEffects'));
+  if (savedState) {
+    console.log(`Local Storage menu settings: ${JSON.stringify(savedState)}`);
+    Object.assign(activeEffects, savedState);
+  }
+
   const toggles = document.querySelectorAll('.toggleButton');
   toggles.forEach((toggle) => {
+    // Apply saved state to toggles
+    if (activeEffects[toggle.id]) {
+      toggle.style.color = 'white';
+      const sceneFunc = idMap[toggle.id];
+      if (sceneFunc) {
+        if (toggle.id === 'dayNightToggle') {
+          dayNight(renderer, scene, 'on');
+        } else {
+          sceneFunc(scene, 'on');
+        }
+      }
+    } else {
+      toggle.style.color = 'grey';
+    }
+
     toggle.addEventListener('click', () => {
       // Check to not have rain/snow on at the same time
       if (toggle.id === 'rainToggle' && activeEffects['snowToggle']) {
@@ -66,6 +88,7 @@ export function menuScenesSelector(scene, renderer) {
           if (toggle.id === 'dayNightToggle' && activeEffects['dayNightToggle'] === true) {
             dayNight(renderer, scene, 'off');
             activeEffects['dayNightToggle'] = false;
+            saveState();
             return;
           }
           // Turn off the effect
@@ -75,13 +98,20 @@ export function menuScenesSelector(scene, renderer) {
           if (toggle.id === 'dayNightToggle' && activeEffects['dayNightToggle'] === false) {
             dayNight(renderer, scene, 'on');
             activeEffects['dayNightToggle'] = true;
+            saveState();
             return;
           }
           // Turn on the effect
           sceneFunc(scene, 'on');
           activeEffects[toggle.id] = true;
         }
+        saveState();
       }
     });
   });
+
+  // Function to save state to local storage
+  function saveState() {
+    localStorage.setItem('activeEffects', JSON.stringify(activeEffects));
+  }
 }

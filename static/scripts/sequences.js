98456f73d39b38cfb9adf5b9758a7f8fe88282d8
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-08-14 11:08:10 -0500

    Hopefully no merge conflict :)

--- static/scripts/sequences.js
+++ static/scripts/sequences.js
@@ -1,8 +1,21 @@
+export function setSynthState(noteArray) {
+  // Load saved state from local storage
+  const synthState = JSON.parse(localStorage.getItem('synthNotes'));
+  // Find which sequence to use
+  const sequenceToUse = noteArray || synthState || Array(8).fill(false);
+  localStorage.setItem('synthNotes', JSON.stringify(sequenceToUse));
+  console.log(`sequenceToUse set to: ${sequenceToUse}`);
+
+  // buttonsArray = document.querySelectorAll('a');
+  return sequenceToUse;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   let fastBtn = document.getElementById('fast');
   let chillBtn = document.getElementById('chill');
   let spaceBtn = document.getElementById('space');
 
+  setSynthState();
   const buttonState = {
     'fast': false,
     'chill': false,
